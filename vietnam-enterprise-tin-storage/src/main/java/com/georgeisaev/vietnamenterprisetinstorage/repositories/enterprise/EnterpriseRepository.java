f2e8fbcb3caa8e0f16d8304dc951195b9c585d65
igeo <georgeisaev@gmail.com>
2021-01-30 18:48:43 +0700

    add persistence layer

--- vietnam-enterprise-tin-storage/src/main/java/com/georgeisaev/vietnamenterprisetinstorage/repositories/enterprise/EnterpriseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.georgeisaev.vietnamenterprisetinstorage.repositories.enterprise;
-
-import com.georgeisaev.vietnamenterprisetinstorage.domain.enterprise.Enterprise;
-import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
-
-public interface EnterpriseRepository extends ReactiveCassandraRepository<Enterprise, String> {
-
-}
