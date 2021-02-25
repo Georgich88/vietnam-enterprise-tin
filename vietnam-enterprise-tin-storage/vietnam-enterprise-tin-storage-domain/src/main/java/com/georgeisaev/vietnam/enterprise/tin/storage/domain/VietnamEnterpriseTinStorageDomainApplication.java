164d377565c6ee465dfc42616b9f057eff72eb90
igeo <georgeisaev@gmail.com>
2021-02-25 14:38:14 +0700

    module for domain

---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-domain/src/main/java/com/georgeisaev/vietnam/enterprise/tin/storage/domain/VietnamEnterpriseTinStorageDomainApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietnamEnterpriseTinStorageDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietnamEnterpriseTinStorageDomainApplication.class, args);
+	}
+
+}
---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-domain/src/test/java/com/georgeisaev/vietnam/enterprise/tin/storage/domain/VietnamEnterpriseTinStorageDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietnamEnterpriseTinStorageDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
