401bf0cd476b0aa6409bab0e50bba63740b48193
igeo <georgeisaev@gmail.com>
2021-02-25 14:58:01 +0700

    module for parser

---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-parsers/src/main/java/com/georgeisaev/vietnam/enterprise/tin/storage/parsers/VietnamEnterpriseTinStorageParsersApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage.parsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietnamEnterpriseTinStorageParsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietnamEnterpriseTinStorageParsersApplication.class, args);
+	}
+
+}
---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-parsers/src/test/java/com/georgeisaev/vietnam/enterprise/tin/storage/parsers/VietnamEnterpriseTinStorageParsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage.parsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietnamEnterpriseTinStorageParsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
