814defab0836fc5dd5b2959688e069f0dcc16ae7
igeo <georgeisaev@gmail.com>
2021-02-25 23:49:43 +0700

    module for frontend, services and web

---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-security/src/test/java/com/georgeisaev/vietnam/enterprise/tin/storage/VietnamEnterpriseTinStorageSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietnamEnterpriseTinStorageSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
