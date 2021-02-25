6e4ad8c7da2380a8bd04a647878b819cdf0a33f2
igeo <georgeisaev@gmail.com>
2021-02-25 15:02:36 +0700

    module for dao layer

---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-dao/src/main/java/com/georgeisaev/vietnam/enterprise/tin/storage/VietnamEnterpriseTinStorageDaoApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietnamEnterpriseTinStorageDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VietnamEnterpriseTinStorageDaoApplication.class, args);
+	}
+
+}
---
+++ vietnam-enterprise-tin-storage/vietnam-enterprise-tin-storage-dao/src/test/java/com/georgeisaev/vietnam/enterprise/tin/storage/VietnamEnterpriseTinStorageDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.vietnam.enterprise.tin.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietnamEnterpriseTinStorageDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
